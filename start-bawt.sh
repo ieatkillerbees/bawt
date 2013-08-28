fcffea7db784de36da8bd3c3732ad1ecdf24f220
Samantha Quinones <squinones@politico.com>
2013-08-28 17:21:08 -0400

    Added hipchat adapter

---
+++ start-bawt.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export HUBOT_HIPCHAT_JID=60295_415067@chat.hipchat.com
+export HUBOT_HIPCHAT_PASSWORD=dasbot!!
+export HUBOT_HIPCHAR_ROOMS=60295_lounge@conf.hipchat.com
+
+bin/hubot --adapter hipchat --name Bawt
